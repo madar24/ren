b42ef48d9f66d17d34b187c6ab75e3bf261787f9
madar24 <ahoy-washtub-gauze@duck.com>
2026-04-10 22:38:15 +0530

    Update __init__.py

--- Backend/fastapi/__init__.py
+++ Backend/fastapi/__init__.py
@@ -6,6 +6,8 @@ Port = Telegram.PORT
 config = uvicorn.Config(
     app=app, 
     host='0.0.0.0', 
-    port=Port
+    port=Port,
+    ssl_keyfile="/root/ren/singapore-madar24.taileb71f2.ts.net.key",
+    ssl_certfile="/root/ren/singapore-madar24.taileb71f2.ts.net.crt"
 )
 server = uvicorn.Server(config)
